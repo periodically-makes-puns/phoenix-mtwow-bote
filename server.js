bb68d5048bed7e82520e0e1b878a49c1e75a08fa
Eric Yang <EricUBN@eyang10000@gmail.com>
2019-10-13 16:45:24 -0400

    Set up SQLite database

---
+++ server.js
@@ -0,0 +1,45 @@
+// Require necessary modules
+const discord = require("discord.js");
+const express = require("express");
+const fs = require("fs");
+const debug = require("debug");
+const sqlAdmin = require("./sqlAdmin.js");
+
+// Initialise loggers
+// Using global because... I know it's bad design but I'm the only asshat coding this thing
+global.debugExpress = debug("express");
+global.debugDiscord = debug("discord");
+global.debugSQLite = debug("sqlite3");
+global.sqlite3 = require("sqlite3");
+
+// Initialise database
+global.db = new sqlite3.Database("data.db");
+sqlAdmin.initialize();
+debugSQLite("Initialised database!");
+
+// Initialise clients
+global.app = express()
+global.client = new discord.Client();
+global.instanceData = JSON.parse(fs.readFileSync("./data.json")); // Read in instance data
+
+/* DISCORD SECTION */
+// Info message when ready
+client.on("ready", () => {
+    debugDiscord("Ready!");
+});
+
+// Login with token
+client.login(instanceData.token);
+
+/* EXPRESS SECTION */
+app.use(express.static('public'))
+
+app.get('/', (req, res, next) => {
+    res.send("Hello world!");
+});
+
+// Info message when ready
+// Listen on port specified in config
+app.listen(instanceData.portNum, () => {
+    debugExpress("Ready!");
+});
---
+++ sqlAdmin.js
@@ -0,0 +1,41 @@
+module.exports.initialize = () => {
+    // Initialises all the tables
+    db.serialize(function() {
+        db.run(`CREATE TABLE IF NOT EXISTS Members (
+                id INT PRIMARY KEY NOT NULL,
+                aggregateVoteCount INT DEFAULT 0,
+                roundVoteCount INT DEFAULT 0
+            );
+        `);
+        db.run(`CREATE TABLE IF NOT EXISTS Contestants (
+                id INT PRIMARY KEY NOT NULL,
+                alive BOOLEAN DEFAULT 0,
+                allowedResponseCount INT DEFAULT 1,
+                responseCount INT DEFAULT 0
+            );
+        `);
+        db.run(`CREATE TABLE IF NOT EXISTS Votes (
+                uid INT PRIMARY KEY NOT NULL,
+                vid INT,
+                gseed STRING,
+                vote STRING
+            );
+        `);
+        db.run(`CREATE TABLE IF NOT EXISTS Responses (
+                uid INT PRIMARY KEY NOT NULL,
+                rid INT,
+                response STRING
+            );
+        `);
+    });
+}
+
+module.exports.fullReset = () => {
+    // Wipes all data
+    db.serialize(function() {
+        db.run(`DROP TABLE IF EXISTS Members;`); 
+        db.run(`DROP TABLE IF EXISTS Contestants;`); 
+        db.run(`DROP TABLE IF EXISTS Votes;`); 
+        db.run(`DROP TABLE IF EXISTS Responses;`); 
+    });
+}
